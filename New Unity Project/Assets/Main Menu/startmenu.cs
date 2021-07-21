bf559675ffab45a53805f59c3de4c9c72467a495
Amcabay <amca.raftan@gmail.com>
2021-07-21 12:05:57 +0700

    NEW STAGE (part 1)
    Bersama banteng bismillah sukses

---
+++ New Unity Project/Assets/Main Menu/startmenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void play(string scene_name)
+   {
+       Application.LoadLevel(scene_name);
+   }
+}
---
+++ New Unity Project/Assets/stage/LandGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandGenerator : MonoBehaviour
+{
+    public GameObject theLand;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float landWidth;
+    void Start()
+    {
+        landWidth = theLand.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + landWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate(theLand, transform.position, transform.rotation);
+        }
+    }
+}
